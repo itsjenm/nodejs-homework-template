f9826caf5123fc73b39ae7fd634a4a933c0e4b9d
Jenny <77757004+itsjenm@users.noreply.github.com>
2023-08-02 21:22:41 -0500

    added conditional to removeContact api

--- controllers/contacts/removeContact.js
+++ controllers/contacts/removeContact.js
@@ -2,14 +2,22 @@ const { contacts: service } = require("../../services");
 
 const removeContact = async (req, res, next) => {
     const contact = await service.removeContact(req.params.id);
-    res.status(201).json({
-        status: 'success',
-        code: 201,
-        message: 'User was deleted',
-        data: {
-            result: contact,
-        }
-    });
+    if (contact) {
+        res.status(201).json({
+            status: 'success',
+            code: 201,
+            message: 'User was deleted',
+            data: {
+                result: contact,
+            }
+        });
+    } else {
+        res.status(404).json({
+            status: 'failed',
+            code: 404, 
+            message: 'Not found',
+        })
+    }
 };
 
 module.exports = removeContact;
